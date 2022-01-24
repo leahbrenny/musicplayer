fa359c285551afef68f32cc92b1a29ddf52ef36c
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2022-01-24 13:52:22 -0600

    added musicMenu with favorites and playlist

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -36,12 +36,17 @@ const navMenu = [
 ];
 
 const musicMenu = [
-    {
-        name:'Create Playlist',
-        icon: MdPlaylistAdd,
-        route: '/'
-    }
-]
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdPlaylistAdd,
+    route: "/favorites",
+  },
+];
 
 const Sidebar = () => {
   return (
@@ -59,6 +64,26 @@ const Sidebar = () => {
         <Box marginBottom="20px">
           <List spacing={2}>
             {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box>
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
               <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
                 <LinkBox>
                   <NextLink href={menu.route} passHref>
